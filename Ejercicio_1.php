6090b2b79d78d870d97835bdf0bcc98ed224cc8b
Cyber War Games Productions <124847965+SuperMarianWorld@users.noreply.github.com>
2023-10-27 03:43:41 -0300

    Add files via upload
    Primeras líneas de php.
    Uso del 'echo' y el 'print'

---
+++ Ejercicio_1.php
@@ -0,0 +1,13 @@
+<?php
+
+//De ésta manera es como se realiza el comentario en PHP pero en una sola línea.
+
+/*
+  Para los comentarios largos de varias líneas se realiza de ésta forma
+  Permitiendo continuar en la siguiente línea y así evitar un código desprolijo.
+*/
+
+echo "prueba con comando echo";
+print_r("prueba con comando print_r");
+print("prueba con comando print simple");
+?>
