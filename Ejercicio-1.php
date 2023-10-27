752b399ae781a2246bfe8896b4ed279a7c626367
Cyber War Games Productions <124847965+SuperMarianWorld@users.noreply.github.com>
2023-10-27 03:37:53 -0300

    Update Ejercicio-1.php
    Corrección de errores sintácticos

--- Ejercicio-1.php
+++ Ejercicio-1.php
@@ -1,11 +1,11 @@
 <?php
 $nombre = $_REQUEST['name-surname'];
 $email = $_REQUEST['mail'];
-$mensaje = $_REQUEST['Mensaje']
+$mensaje = $_REQUEST['Mensaje'];
 
-echo"El nombre ingresado es: $nombre"
-echo"El email ingresado es: $email"
-echo"Su mensaje enviado: $mensaje"
+echo "El nombre ingresado es: $nombre";
+echo "El email ingresado es: $email";
+echo "Su mensaje enviado: $mensaje";
 
 /*La integración del archivo php de éste documento se realiza mediante la invocación del archivo en el documento HTML 
   llamado Index.html
