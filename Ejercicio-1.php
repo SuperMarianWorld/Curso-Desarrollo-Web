754426252b400764b82ddd7c7da21ed8ef5f7e37
Cyber War Games Productions <124847965+SuperMarianWorld@users.noreply.github.com>
2023-10-26 21:44:18 -0300

    Add files via upload
    Este es un ejemplo simple de la forma en la que se puede integrar un documento php en uno de html.

---
+++ Ejercicio-1.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_REQUEST['name-surname'];
+$email = $_REQUEST['mail'];
+$mensaje = $_REQUEST['Mensaje']
+
+echo"El nombre ingresado es: $nombre"
+echo"El email ingresado es: $email"
+echo"Su mensaje enviado: $mensaje"
+
+?>
