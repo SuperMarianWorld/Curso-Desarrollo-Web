ec6da98566687b94a1620748d49a25dd97f95684
Cyber War Games Productions <124847965+SuperMarianWorld@users.noreply.github.com>
2023-10-27 01:25:13 -0300

    Update Ejercicio-1.php

--- Ejercicio-1.php
+++ Ejercicio-1.php
@@ -7,4 +7,7 @@ echo"El nombre ingresado es: $nombre"
 echo"El email ingresado es: $email"
 echo"Su mensaje enviado: $mensaje"
 
+/*La integración del archivo php de éste documento se realiza mediante la invocación del archivo en el documento HTML 
+  llamado Index.html
+*/
 ?>
